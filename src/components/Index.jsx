4a2a817da9726430c5f4f4db4dfa96ee2314584c
Darshan Unagar <160447032+Darshan-unagar@users.noreply.github.com>
2024-07-26 15:42:57 +0530

    Update Index.jsx

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -14,7 +14,7 @@ function Index() {
     try {
         console.log('Sending request with data:', { name, email, message });
   
-      const response = await fetch('https://darshanunagar.vercel.app/api/contact', {
+      const response = await fetch('https://darshanunagar.vercel.app', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
